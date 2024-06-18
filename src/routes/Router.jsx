ba21c0abaa006927d6cd8360a39c6d15e347e435
TitadachFF <titadach15632441@gmail.com>
2024-06-18 08:59:56 +0700

    studentinfo from

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home";
 import Main from "../layout/Main";
 import StudentMenu from "../Student/StudentMenu";
 import AdminMenu from "../Admin/AdminMenu";
+import StudentInfo from "../Student/StudentInfo";
 
 const Router = createBrowserRouter ([
   {
@@ -17,6 +18,10 @@ const Router = createBrowserRouter ([
       {
         path: "/student",
         element: <StudentMenu />,
+      },
+      {
+        path: "/studentinfo",
+        element: <StudentInfo />,
       },
       {
         path: "/admin",
