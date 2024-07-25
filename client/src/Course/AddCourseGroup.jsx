22077e18da62f7b6dce726954e5310da16893944
TitadachFF <titadach15632441@gmail.com>
2024-07-26 03:10:12 +0700

    make add course 50%

--- client/src/Course/AddCourseGroup.jsx
+++ client/src/Course/AddCourseGroup.jsx
@@ -1,118 +1,136 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const AddCourseGroup = () => {
-     const navigate = useNavigate();
-  const [groups, setgroups] = useState([
-    { id: 1, name: "หมวดวิชาศึกษาทั่วไป", },
-    { id: 2, name: "หมวดวิชาแกน", },
-  ]);
+  const navigate = useNavigate();
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [groupName, setGroupName] = useState("");
 
-  const [selectedCourse, setSelectedCourse] = useState("");
-  const [instructorName, setInstructorName] = useState("");
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const token = localStorage.getItem("token");
 
-  const handleCourseChange = (e) => {
-    setSelectedCourse(e.target.value);
+        const response = await fetch("http://localhost:3000/api/getAllCategories", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = await response.json();
+        setCategories(data.categories || []); // กำหนดค่า default เป็นอาร์เรย์ว่าง
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
   };
 
-  const handleInstructorNameChange = (e) => {
-    setInstructorName(e.target.value);
+  const handleGroupNameChange = (e) => {
+    setGroupName(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     console.log(
-      `Selected Course: ${selectedCourse}, Instructor Name: ${instructorName}`
+      `Selected Category: ${selectedCategory}, Group Name: ${groupName}`
     );
   };
+
   const updateQueryString = (form) => {
     navigate(`?form=${form}`);
   };
+
   return (
     <form onSubmit={handleSubmit}>
-              <div className="mb-2">
-                <label className="mb-2">เลือกหมวดวิชา</label>
-              </div>
-              <div className="relative mb-6">
-                <select
-                  id="class"
-                  className="dropdown appearance-none text-gray-500 w-30 mt-1 bg-white border border-gray-300 rounded-lg py-2 pl-4 pr-8 leading-tight focus:outline-none focus:border-gray-500"
-                  value={selectedCourse}
-                  onChange={handleCourseChange}
-                >
-                  <option value="">เลือกหมวดวิชา</option>
-                  {groups.map((course) => (
-                    <option key={course.id} value={course.id}>
-                      {course.name}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div className="grid grid-cols-3 gap-4 mb-4">
-                <div className="flex flex-col col-span-2">
-                  <label className="mb-2">ชื่อกลุ่มวิชา</label>
-                  <input
-                    type="text"
-                    className="border rounded-lg px-2 py-2"
-                    value={instructorName}
-                    onChange={handleInstructorNameChange}
-                  />
-                </div>
-                <div className="flex flex-col">
-                  <label className="mb-2">จำนวนหน่วยกิต</label>
-                  <input
-                    type="number"
-                    className="border rounded-lg px-2 py-2"
-                  />
-                </div>
-              </div>
+      <div className="mb-2">
+        <label className="mb-2">เลือกหมวดวิชา</label>
+      </div>
+      <div className="relative mb-6">
+        <select
+          id="category"
+          className="dropdown appearance-none text-gray-500 w-30 mt-1 bg-white border border-gray-300 rounded-lg py-2 pl-4 pr-8 leading-tight focus:outline-none focus:border-gray-500"
+          value={selectedCategory}
+          onChange={handleCategoryChange}
+        >
+          <option value="">เลือกหมวดวิชา</option>
+          {Array.isArray(categories) &&
+            categories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.categoryName}
+              </option>
+            ))}
+        </select>
+      </div>
+      <div className="grid grid-cols-3 gap-4 mb-4">
+        <div className="flex flex-col col-span-2">
+          <label className="mb-2">ชื่อกลุ่มวิชา</label>
+          <input
+            type="text"
+            className="border rounded-lg px-2 py-2"
+            value={groupName}
+            onChange={handleGroupNameChange}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="mb-2">จำนวนหน่วยกิต</label>
+          <input
+            type="number"
+            className="border rounded-lg px-2 py-2"
+          />
+        </div>
+      </div>
 
-              <div className="mt-6 flex justify-between">
-                <button
-                  type="button"
-                  className="px-6 py-2 bg-gray-100 border border-red-600 text-red-600 rounded"
-                  onClick={() => updateQueryString("addCourseCategory")}
-                >
-                  ย้อนกลับ
+      <div className="mt-6 flex justify-between">
+        <button
+          type="button"
+          className="px-6 py-2 bg-gray-100 border border-red-600 text-red-600 rounded"
+          onClick={() => updateQueryString("addCourseCategory")}
+        >
+          ย้อนกลับ
+        </button>
+        {/* Modal */}
+        <button
+          className="px-8 py-2 bg-red border border-red text-white rounded"
+          onClick={() =>
+            document.getElementById("my_modal_1").showModal()
+          }
+        >
+          บันทึก
+        </button>
+        <dialog id="my_modal_1" className="modal">
+          <div className="modal-box">
+            <h3 className="font-bold text-lg">บันทึกข้อมูลสำเร็จ!</h3>
+            <p className="py-4 text-gray-500">
+              กดปุ่ม ESC หรือ กดปุ่มปิดด้านล่างเพื่อปิด
+            </p>
+            <div className="modal-action flex justify-between">
+              <form
+                method="dialog"
+                className="w-full flex justify-between"
+              >
+                {/* if there is a button in form, it will close the modal */}
+                <button className="px-10 py-2 bg-white text-red border font-semibold border-red rounded">
+                  ปิด
                 </button>
-                {/* Modal */}
                 <button
                   className="px-8 py-2 bg-red border border-red text-white rounded"
-                  onClick={() =>
-                    document.getElementById("my_modal_1").showModal()
-                  }
+                  onClick={() => updateQueryString("addSubject")}
                 >
-                  บักทึก
+                  ถัดไป
                 </button>
-                <dialog id="my_modal_1" className="modal">
-                  <div className="modal-box">
-                    <h3 className="font-bold text-lg">บันทึกข้อมูลสำเร็จ!</h3>
-                    <p className="py-4 text-gray-500">
-                      กดปุ่ม ESC หรือ กดปุ่มปิดด้านล้างเพื่อปิด
-                    </p>
-                    <div className="modal-action flex justify-between">
-                      <form
-                        method="dialog"
-                        className="w-full flex justify-between"
-                      >
-                        {/* if there is a button in form, it will close the modal */}
-                        <button className="px-10 py-2 bg-white text-red border font-semibold border-red rounded">
-                          ปิด
-                        </button>
-                        <button
-                          className="px-8 py-2 bg-red border border-red text-white rounded"
-                          onClick={() => updateQueryString("addSubject")}
-                        >
-                          ถัดไป
-                        </button>
-                      </form>
-                    </div>
-                  </div>
-                </dialog>
-                {/* End Modal */}
-              </div>
-            </form>
+              </form>
+            </div>
+          </div>
+        </dialog>
+        {/* End Modal */}
+      </div>
+    </form>
   );
 };
 
--- client/src/Course/AddCourseName.jsx
+++ client/src/Course/AddCourseName.jsx
@@ -2,13 +2,51 @@ import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
 const AddCourseName = () => {
-
   const navigate = useNavigate();
   const location = useLocation();
-  const [instructorNames, setInstructorNames] = useState([""]);
+  const [formData, setFormData] = useState({
+    majorNameTH: "",
+    majorNameENG: "",
+    majorYear: "",
+    majorUnit: "",
+    majorCode: "",
+    majorStatus: "",
+    majorSupervisor: "",
+  });
 
-  const handleSubmit = (e) => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    const token = localStorage.getItem("token");
+    
+    try {
+      const token = localStorage.getItem("token");
+
+      const response = await fetch("http://localhost:3000/api/createMajor", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ major: formData }),
+      });
+      
+      if (response.ok) {
+        document.getElementById("my_modal_1").showModal();
+      } else {
+        const errorData = await response.json();
+        console.error("Error response:", errorData);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   const getQueryStringValue = (key) => {
@@ -16,71 +54,91 @@ const AddCourseName = () => {
   };
 
   const currentForm = getQueryStringValue("form") || "addCourse";
-  
+
   const updateQueryString = (form) => {
     navigate(`?form=${form}`);
   };
 
-  
-  const handleInstructorNameChange = (index, e) => {
-    const newInstructorNames = [...instructorNames];
-    newInstructorNames[index] = e.target.value;
-    setInstructorNames(newInstructorNames);
-  };
-
-  const addInstructorField = () => {
-    setInstructorNames([...instructorNames, ""]);
-  };
-
   return (
     <div>
-      {" "}
       <form onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 gap-4 mb-4">
           <div className="flex flex-col">
             <label className="mb-2">ชื่อหลักสูตร(ภาษาไทย)</label>
-            <input type="text" className="border rounded-lg px-2 py-2" />
+            <input
+              type="text"
+              name="majorNameTH"
+              className="border rounded-lg px-2 py-2"
+              value={formData.majorNameTH}
+              onChange={handleChange}
+            />
           </div>
           <div className="flex flex-col">
             <label className="mb-2">ชื่อหลักสูตร(ภาษาอังกฤษ)</label>
-            <input type="text" className="border rounded-lg px-2 py-2" />
+            <input
+              type="text"
+              name="majorNameENG"
+              className="border rounded-lg px-2 py-2"
+              value={formData.majorNameENG}
+              onChange={handleChange}
+            />
           </div>
         </div>
 
         <div className="grid grid-cols-3 gap-4 mb-4">
           <div className="flex flex-col">
             <label className="mb-2">รหัสหลักสูตร</label>
-            <input type="text" className="border rounded-lg px-2 py-2" />
+            <input
+              type="text"
+              name="majorCode"
+              className="border rounded-lg px-2 py-2"
+              value={formData.majorCode}
+              onChange={handleChange}
+            />
           </div>
           <div className="flex flex-col">
             <label className="mb-2">หลักสูตร ปี</label>
-            <input type="text" className="border rounded-lg px-2 py-2" />
+            <input
+              type="text"
+              name="majorYear"
+              className="border rounded-lg px-2 py-2"
+              value={formData.majorYear}
+              onChange={handleChange}
+            />
           </div>
           <div className="flex flex-col">
             <label className="mb-2">จำนวนหน่วยกิต</label>
-            <input type="number" className="border rounded-lg px-2 py-2" />
+            <input
+              type="number"
+              name="majorUnit"
+              className="border rounded-lg px-2 py-2"
+              value={formData.majorUnit}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="mb-2">สถานะ</label>
+            <input
+              type="text"
+              name="majorStatus"
+              className="border rounded-lg px-2 py-2"
+              value={formData.majorStatus}
+              onChange={handleChange}
+            />
           </div>
         </div>
-        {instructorNames.map((name, index) => (
-              <div key={index}>
-                <label className="mb-2">อาจารย์ผู้รับผิดชอบหลักสูตร</label>
-                <input
-                  type="text"
-                  className="border rounded-lg px-2 py-2 mb-2 mt-2 block w-full"
-                  value={name}
-                  placeholder="ชื่อ-สกุล อาจารย์"
-                  onChange={(e) => handleInstructorNameChange(index, e)}
-                />
-              </div>
-            ))}
 
-<button type="button" className="px-4 py-2 bg-base border border-red text-red rounded mt-2 hover:bg-gray-100" onClick={addInstructorField}>
-              เพิ่มอาจารย์ผู้รับผิดชอบหลักสูตร +
-            </button>
-    
-
-
-
+        <div>
+          <label className="mb-2">อาจารย์ผู้รับผิดชอบหลักสูตร</label>
+          <input
+            type="text"
+            name="majorSupervisor"
+            className="border rounded-lg px-2 py-2 mb-2 mt-2 block w-full"
+            placeholder="ชื่อ-สกุล อาจารย์"
+            value={formData.majorSupervisor}
+            onChange={handleChange}
+          />
+        </div>
 
         <div className="mt-6 flex justify-between">
           <button
@@ -90,38 +148,36 @@ const AddCourseName = () => {
           >
             ย้อนกลับ
           </button>
-          {/* Modal */}
           <button
+            type="submit"
             className="px-8 py-2 bg-red border border-red text-white rounded"
-            onClick={() => document.getElementById("my_modal_1").showModal()}
           >
-            บักทึก
+            บันทึก
           </button>
-          <dialog id="my_modal_1" className="modal">
-            <div className="modal-box">
-              <h3 className="font-bold text-lg">บันทึกข้อมูลสำเร็จ!</h3>
-              <p className="py-4 text-gray-500">
-                กดปุ่ม ESC หรือ กดปุ่มปิดด้านล้างเพื่อปิด
-              </p>
-              <div className="modal-action flex justify-between">
-                <form method="dialog" className="w-full flex justify-between">
-                  {/* if there is a button in form, it will close the modal */}
-                  <button className="px-10 py-2 bg-white text-red border font-semibold border-red rounded">
-                    ปิด
-                  </button>
-                  <button
-                    className="px-8 py-2 bg-red border border-red text-white rounded"
-                    onClick={() => updateQueryString("addTeacher")}
-                  >
-                    ถัดไป
-                  </button>
-                </form>
-              </div>
-            </div>
-          </dialog>
-          {/* End Modal */}
         </div>
       </form>
+
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">บันทึกข้อมูลสำเร็จ!</h3>
+          <p className="py-4 text-gray-500">
+            กดปุ่ม ESC หรือ กดปุ่มปิดด้านล่างเพื่อปิด
+          </p>
+          <div className="modal-action flex justify-between">
+            <form method="dialog" className="w-full flex justify-between">
+              <button className="px-10 py-2 bg-white text-red border font-semibold border-red rounded">
+                ปิด
+              </button>
+              <button
+                className="px-8 py-2 bg-red border border-red text-white rounded"
+                onClick={() => updateQueryString("addCourseCategory")}
+              >
+                ถัดไป
+              </button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
